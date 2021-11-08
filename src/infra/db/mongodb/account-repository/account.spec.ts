7b98f33b4b48adfa7728dde190d5f4feeafb4871
Alexandre de Oliveira Pereira <alexandre.olipe@gmail.com>
2021-11-08 16:07:13 -0300

    test: eclear database between tests

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -6,6 +6,11 @@ describe('Account MongoDB Repository', () => {
     await MongoHelper.connect()
   })
 
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   afterAll(async () => {
     await MongoHelper.disconnect()
   })
